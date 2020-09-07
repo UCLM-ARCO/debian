ecceccfa4dbed9c858102e3a21920fc642145c7c
David Villa Alises <david.villa@gmail.com>
2020-09-07 13:52:54 +0200

    rename

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash --
 git pull
 git add docs/*
-message=$(git status -s | grep ^A | grep .dsc$ | python3 -c "import pathlib; print(pathlib.Path(input()).stem)")
-git ci -a -m "upload $message"
+package=$(git status -s | grep ^A | grep .dsc$ | python3 -c "import pathlib; print(pathlib.Path(input()).stem)")
+git ci -a -m "upload $package"
 git push
