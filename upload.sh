298de951deaf6366a49634523a2e411624dd0ecd
David Villa Alises <david.villa@gmail.com>
2020-09-08 16:02:25 +0200

    upload

--- upload.sh
+++ upload.sh
@@ -2,5 +2,6 @@
 git pull
 git add docs/*
 package=$(git status -s | grep ^A | grep .dsc$ | python3 -c "import pathlib; print(pathlib.Path(input()).stem)")
+echo "git commiting package $package"
 git ci -a -m "upload $package"
 git push
