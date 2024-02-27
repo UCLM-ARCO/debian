67764b509d89837ba0064ad9fdd7608eecd42ff0
David Villa Alises <david.villa@gmail.com>
2024-02-27 20:11:14 +0100

    add sources.list

--- docs/add-repo.sh
+++ docs/add-repo.sh
@@ -2,3 +2,5 @@
 
 apt -y install wget
 wget -P /etc/apt/trusted.gpg.d https://uclm-arco.github.io/debian/uclm-arco.asc
+echo "deb https://uclm-arco.github.io/debian/ sid main" > /etc/apt/sources.list.d/arco.list
+apt update
